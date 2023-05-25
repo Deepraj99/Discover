6fefd63bf24e713a511606c4a9363a30c1249f41
Deepak Prajapati <dp0774138@gmail.com>
2023-05-25 18:09:02 +0530

    Navigation component

--- app/src/main/java/com/example/discover/MainActivity.kt
+++ app/src/main/java/com/example/discover/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.discover
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.discover.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, findNavController(R.id.navHostFragment))
     }
 }
