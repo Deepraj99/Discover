74a501915ac262b8c12289fd04d485857ccd5a4b
Deepak Prajapati <dp0774138@gmail.com>
2023-05-29 11:12:10 +0530

    All news done

---
+++ app/src/main/java/com/example/discover/adapters/AllNewsAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.discover.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.discover.R
+import com.example.discover.models.Article
+import com.example.discover.utils.NewsDiffUtil
+
+class AllNewsAdapter: RecyclerView.Adapter<AllNewsAdapter.MyViewHolder>() {
+    private lateinit var view: View
+    private var oldNewsList = emptyList<Article>()
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ivBackground = view.findViewById<ImageView>(R.id.iv_background)!!
+        val tvSource = view.findViewById<TextView>(R.id.tv_source)!!
+        val tvHeading = view.findViewById<TextView>(R.id.tv_heading)!!
+        val tvAuthor = view.findViewById<TextView>(R.id.tv_author)!!
+        val tvDate = view.findViewById<TextView>(R.id.tv_date)!!
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        view = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return oldNewsList.count()
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.tvSource.text = oldNewsList[position].source.name
+        holder.tvHeading.text = oldNewsList[position].title
+        holder.tvAuthor.text = oldNewsList[position].author
+        holder.tvDate.text = oldNewsList[position].publishedAt.substring(0, 10)
+        Glide.with(view).load(oldNewsList[position].urlToImage).centerCrop().placeholder(R.drawable.no_image_avaliable).into(holder.ivBackground)
+    }
+
+    fun setData(newNewsList: List<Article>) {
+        val diffUtil = NewsDiffUtil(oldNewsList, newNewsList)
+        val diffResults = DiffUtil.calculateDiff(diffUtil)
+        oldNewsList = newNewsList
+        diffResults.dispatchUpdatesTo(this)
+    }
+}
--- app/src/main/java/com/example/discover/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/discover/fragments/HomeFragment.kt
@@ -1,14 +1,15 @@
 package com.example.discover.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.discover.activities.NewsApplication
+import com.example.discover.adapters.AllNewsAdapter
 import com.example.discover.databinding.FragmentHomeBinding
 import com.example.discover.viewModels.NewsViewModel
 import com.example.discover.viewModels.NewsViewModelFactory
@@ -17,6 +18,8 @@ class HomeFragment : Fragment() {
 
     private lateinit var newsViewModel: NewsViewModel
     private lateinit var binding: FragmentHomeBinding
+    private val allNewsAdapter by lazy { AllNewsAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,23 +30,22 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val newsRepository = (activity?.applicationContext as NewsApplication).newsRepository
+        newsViewModel = ViewModelProvider(this, NewsViewModelFactory(newsRepository))[NewsViewModel::class.java]
+
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = allNewsAdapter
+
+        newsViewModel.allNewsData.observe(viewLifecycleOwner, Observer {
+            allNewsAdapter.setData(it.articles)
+        })
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val newsRepository = (activity?.applicationContext as NewsApplication).newsRepository
-        newsViewModel = ViewModelProvider(this, NewsViewModelFactory(newsRepository))[NewsViewModel::class.java]
-
-        newsViewModel.topHeadlinesData.observe(viewLifecycleOwner, Observer {
-            val data = it.articles.size
-            Log.d("DEEPAK1", data.toString())
-        })
-
-        newsViewModel.allNewsData.observe(viewLifecycleOwner, Observer {
-            Log.d("DEEPAK2", it.articles.size.toString())
-        })
 
     }
 }
