a79875804914bc120ec261441a1a4b1b0fa38925
Deepak Prajapati <dp0774138@gmail.com>
2023-06-02 16:03:21 +0530

    few changes

--- app/src/main/java/com/example/discover/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/discover/fragments/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.example.discover.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +26,8 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         setUpTabLayoutWithViewPager()
+        binding.ivSearch.setOnClickListener { Log.d("DEEPAK", binding.searchBar.text.toString()) }
+
         return binding.root
     }
 
